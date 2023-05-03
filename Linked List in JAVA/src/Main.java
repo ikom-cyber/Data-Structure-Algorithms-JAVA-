2009295182ee86281d450103cb1a8bbfcc544b9a
Ikom Virk <ikomvirk@gmail.com>
2023-05-03 16:31:44 +0530

    linkedlist update

--- Linked List in JAVA/src/Main.java
+++ Linked List in JAVA/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        /*linkedList.push("A");
+        linkedList.push("B");
+        linkedList.push("C");
+        linkedList.push("D");
+        linkedList.push("E");*/
+
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("E");
+
+        linkedList.add(4,"E");
+        System.out.println(linkedList);
+
     }
 }
